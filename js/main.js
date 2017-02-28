ab2c5e5b4b3f30fb1a18bc54de2bc764a3c2525b
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-02-27 17:10:50 -0700

    Addition for initial project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!");
+
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "King";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
