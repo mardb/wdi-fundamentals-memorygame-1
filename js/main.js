8323f85857ed97b210267ca89e429717bf9dbbf6
Troy Evans <troy@Troys-MacBook-Pro.local>
2017-03-01 16:05:53 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-console.log("Up and Running!");
-
 var cards = ["queen", "king", "queen", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function(){
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
 	alert("You found a match!");
 } else {
 		alert("Sorry, try again");
 	}
+}
+
+var flipCard = function(cardID){
+	checkForMatch();
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+}
+
+flipCard(0);
+flipCard(1);
